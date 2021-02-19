f15b27e33514e49f9c19687f78cd9922731ef93f
rei-suzuki <tcoxoxol8@gmail.com>
2021-02-19 15:41:49 +0900

    Replace instance from the team library component
    Signed-off-by: rei-suzuki <tcoxoxol8@gmail.com>

--- code.js
+++ code.js
@@ -1,1 +1,44 @@
-throw new Error("This plugin template uses Typescript. Follow the instructions in `README.md` to generate `code.js`.")
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+function main() {
+    return __awaiter(this, void 0, void 0, function* () {
+        if (figma.command == "saveTargetComponent") {
+            yield saveTargetComponent();
+        }
+        else if (figma.command == "replaceNodes") {
+            yield replaceNodes();
+        }
+        figma.closePlugin();
+    });
+}
+// ComponentFileのComponent IDを取得して保存
+function saveTargetComponent() {
+    return __awaiter(this, void 0, void 0, function* () {
+        for (let i = 0; i < figma.currentPage.selection.length; i++) {
+            let nodes = figma.currentPage.selection;
+            yield figma.clientStorage.setAsync("key", nodes[i].key);
+        }
+        return true;
+    });
+}
+// 保存したComponent IDを使ってリプレイス
+function replaceNodes() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const teamLibraryComponentKey = yield figma.clientStorage.getAsync("key");
+        const teamLibraryMasterComponent = yield figma.importComponentByKeyAsync(teamLibraryComponentKey);
+        const selections = figma.currentPage.selection;
+        for (let i = 0; i < selections.length; i++) {
+            if (selections[i].type === "INSTANCE") {
+                selections[i].masterComponent = teamLibraryMasterComponent;
+            }
+        }
+    });
+}
+main();
--- code.ts
+++ code.ts
@@ -5,17 +5,28 @@ async function main() {
   else if (figma.command == "replaceNodes") {
     await replaceNodes()
   }
-  figma.closePlugin()
+      figma.closePlugin()
 }
 
 // ComponentFileのComponent IDを取得して保存
 async function saveTargetComponent() {
-  return 1
+  for (let i = 0; i< figma.currentPage.selection.length; i++) {
+    let nodes = figma.currentPage.selection
+    await figma.clientStorage.setAsync("key", (nodes[i] as ComponentNode).key)
+  }
+  return true
 }
 
 // 保存したComponent IDを使ってリプレイス
 async function replaceNodes() {
-  return 1
+  const teamLibraryComponentKey = await figma.clientStorage.getAsync("key")
+  const teamLibraryMasterComponent = await figma.importComponentByKeyAsync(teamLibraryComponentKey)
+  const selections = figma.currentPage.selection
+  for (let i = 0; i< selections.length; i++) {
+    if (selections[i].type === "INSTANCE") {
+      (selections[i]as InstanceNode).masterComponent = teamLibraryMasterComponent
+    }
+  }
 }
 
 main()
