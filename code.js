1cad421f0278c6a9f0b67f3a2e5073b9a427adae
rei-suzuki <tcoxoxol8@gmail.com>
2021-02-28 20:28:20 +0900

    Replace the target node by recursive

--- code.js
+++ code.js
@@ -11,46 +11,67 @@ const clientStrageKey = 'team-library-components';
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         if (figma.command == "saveTargetComponent") {
-            yield saveTargetComponent([figma.currentPage]);
+            yield figma.clientStorage.setAsync(clientStrageKey, yield saveTargetComponent([figma.currentPage]));
         }
         else if (figma.command == "replaceNodes") {
-            yield replaceNodes(figma.currentPage.selection);
+            const teamLibraryComponents = yield figma.clientStorage.getAsync(clientStrageKey);
+            yield replaceNodes(figma.currentPage.selection, teamLibraryComponents);
         }
         figma.closePlugin();
     });
 }
-// ComponentFileのComponent IDを取得して保存
-const teamLibraryMasterComponents = {};
 function saveTargetComponent(nodes) {
     return __awaiter(this, void 0, void 0, function* () {
-        for (let i = 0; i < nodes.length; i++) {
-            if (nodes[i].type === "COMPONENT" || nodes[i].type === "COMPONENTSET") {
-                teamLibraryMasterComponents[nodes[i].name] = nodes[i].key;
+        const teamLibraryMasterComponents = {};
+        findComponent(teamLibraryMasterComponents, nodes);
+        return teamLibraryMasterComponents;
+    });
+}
+function findComponent(teamLibraryMasterComponents, nodes) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const targetNode = [];
+        for (const node of nodes) {
+            if (node.type === "COMPONENT" || node.type === "COMPONENT_SET") {
+                teamLibraryMasterComponents[node.name] = node.key;
             }
-            if (nodes[i].children != null) {
-                saveTargetComponent(nodes[i].children);
+            if (node.children != null) {
+                findComponent(teamLibraryMasterComponents, node.children);
             }
-            if (nodes[i].children === null) {
+            if (node.children === null) {
                 return false;
             }
         }
-        yield figma.clientStorage.setAsync(clientStrageKey, teamLibraryMasterComponents);
     });
 }
 // 保存したComponent IDを使ってリプレイス
-function replaceNodes(nodes) {
+function replaceNodes(nodes, teamLibraryComponents) {
     return __awaiter(this, void 0, void 0, function* () {
-        const teamLibraryComponents = yield figma.clientStorage.getAsync(clientStrageKey);
         for (let i = 0; i < nodes.length; i++) {
-            if (nodes[i].type === "FRAME" || nodes[i].type === "INSTANCE") {
+            if (nodes[i].type === "INSTANCE" || nodes[i].type === "FRAME") {
                 const key = teamLibraryComponents[nodes[i].name];
-                const getTeamLibraryComponent = yield figma.importComponentByKeyAsync(key);
-                const teamLibrayComponentInstance = yield getTeamLibraryComponent.createInstance();
-                nodes[i].parent.insertChild(1, teamLibrayComponentInstance);
-                //indexの1はまだ適当に入れてるだけ
-                teamLibrayComponentInstance.x = nodes[i].x;
-                teamLibrayComponentInstance.y = nodes[i].y;
-                nodes[i].remove();
+                if (key != undefined) {
+                    try {
+                        const getTeamLibraryComponent = yield figma.importComponentByKeyAsync(key);
+                        const teamLibrayComponentInstance = yield getTeamLibraryComponent.createInstance();
+                        const index = nodes[i].parent.children.findIndex((child) => child.id === nodes[i].id);
+                        nodes[i].parent.insertChild(index, teamLibrayComponentInstance);
+                        teamLibrayComponentInstance.x = nodes[i].x;
+                        teamLibrayComponentInstance.y = nodes[i].y;
+                        nodes[i].remove();
+                    }
+                    catch (e) {
+                        figma.notify(e);
+                    }
+                }
+                else {
+                    replaceNodes(nodes[i].children, teamLibraryComponents);
+                }
+            }
+            else if (nodes[i].children != null) {
+                replaceNodes(nodes[i].children, teamLibraryComponents);
+            }
+            else if (nodes[i].children === null) {
+                return false;
             }
         }
     });
--- code.ts
+++ code.ts
@@ -2,46 +2,62 @@ const clientStrageKey = 'team-library-components'
 
 async function main() {
   if (figma.command == "saveTargetComponent") {
-    await saveTargetComponent([figma.currentPage])
+    await figma.clientStorage.setAsync(clientStrageKey, await saveTargetComponent([figma.currentPage])) 
   }
   else if (figma.command == "replaceNodes") {
-    await replaceNodes(figma.currentPage.selection)
+    const teamLibraryComponents = await figma.clientStorage.getAsync(clientStrageKey)    
+    await replaceNodes(figma.currentPage.selection, teamLibraryComponents)
   }
   figma.closePlugin()
 }
 
-// ComponentFileのComponent IDを取得して保存
-const teamLibraryMasterComponents = {}
 async function saveTargetComponent(nodes) {
-  for (let i = 0; i < nodes.length; i++) {
-    if (nodes[i].type === "COMPONENT" || nodes[i].type === "COMPONENTSET") {
-      teamLibraryMasterComponents[nodes[i].name] = (nodes[i] as ComponentNode).key
+  const teamLibraryMasterComponents = {}
+  findComponent(teamLibraryMasterComponents, nodes)
+
+  return teamLibraryMasterComponents
+}
+
+async function findComponent(teamLibraryMasterComponents, nodes) {
+  const targetNode = []
+  for (const node of nodes) {
+    if (node.type === "COMPONENT" || node.type === "COMPONENT_SET") {
+      teamLibraryMasterComponents[node.name] = node.key
     } 
-    if (nodes[i].children != null) {
-      saveTargetComponent(nodes[i].children)
+    if (node.children != null) {
+      findComponent(teamLibraryMasterComponents, node.children)
     }
-    if (nodes[i].children === null){
+    if (node.children === null){
       return false
     }
-  }
-  
-  await figma.clientStorage.setAsync(clientStrageKey, teamLibraryMasterComponents) 
+  } 
 }
 
 // 保存したComponent IDを使ってリプレイス
-async function replaceNodes(nodes) {
-  const teamLibraryComponents = await figma.clientStorage.getAsync(clientStrageKey)
-
-  for (let i = 0; i< nodes.length; i++) {
-    if (nodes[i].type === "FRAME" || nodes[i].type === "INSTANCE") {
+async function replaceNodes(nodes, teamLibraryComponents) {
+  for (let i = 0; i < nodes.length; i++) {
+    if (nodes[i].type === "INSTANCE" || nodes[i].type === "FRAME") {
       const key = teamLibraryComponents[nodes[i].name]
-      const getTeamLibraryComponent = await figma.importComponentByKeyAsync(key)
-      const teamLibrayComponentInstance = await getTeamLibraryComponent.createInstance()
-      nodes[i].parent.insertChild(1, teamLibrayComponentInstance)
-      //indexの1はまだ適当に入れてるだけ
-      teamLibrayComponentInstance.x = nodes[i].x
-      teamLibrayComponentInstance.y = nodes[i].y
-      nodes[i].remove()
+      if (key != undefined) {
+        try {
+          const getTeamLibraryComponent = await figma.importComponentByKeyAsync(key)
+          const teamLibrayComponentInstance = await getTeamLibraryComponent.createInstance()
+          const index = nodes[i].parent.children.findIndex((child) => child.id === nodes[i].id)
+          nodes[i].parent.insertChild(index, teamLibrayComponentInstance)
+          teamLibrayComponentInstance.x = nodes[i].x
+          teamLibrayComponentInstance.y = nodes[i].y
+          nodes[i].remove()
+        }
+        catch(e) {
+          figma.notify(e)
+        } 
+      } else {
+        replaceNodes(nodes[i].children, teamLibraryComponents)
+      }
+    } else if (nodes[i].children != null) {
+      replaceNodes(nodes[i].children, teamLibraryComponents)
+    } else if (nodes[i].children === null) {
+      return false
     }
   }
 }
